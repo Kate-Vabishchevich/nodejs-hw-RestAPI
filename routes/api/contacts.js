2d56c9a693f67d3892161bad6b726d4b20a376fa
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2023-05-01 11:04:29 +0300

    upd

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
 const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:contactId", ctrl.getContactById);
+// router.get("/:contactId", ctrl.getContactById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:contactId", ctrl.removeContact);
+// router.delete("/:contactId", ctrl.removeContact);
 
-router.put(
-  "/:contactId",
-  validateBody(schemas.updateSchema),
-  ctrl.updateContact
-);
+// router.put(
+//   "/:contactId",
+//   validateBody(schemas.updateSchema),
+//   ctrl.updateContact
+// );
 
-module.exports = router;
+// module.exports = router;
